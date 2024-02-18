e20d89c6885a423a947c25305923f0fd226e8272
Christian <c.espinoza.code@gmail.com>
2024-02-18 14:14:52 -0500

    icon replaced by bug

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,9 +1,10 @@
 import Link from 'next/link';
+import { AiFillBug } from "react-icons/ai";
 
 function NavBar() {
   return (
     <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
-      <Link href="/">Logo</Link>
+      <Link href="/"><AiFillBug /></Link>
       <ul className='flex space-x-6'>
         <li>
           <Link href="/">Dashboard</Link>
