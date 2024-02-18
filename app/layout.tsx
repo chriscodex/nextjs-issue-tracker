8bbcce397d113db3b1f8d0bbab04789d7d0b73c3
Christian <c.espinoza.code@gmail.com>
2024-02-18 11:54:02 -0500

    navbar component added to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
+import { NavBar } from './NavBar';
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -17,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <NavBar />
         <main>{children}</main>
       </body>
     </html>
