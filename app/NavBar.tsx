626fd24d439caa80e284f22dc13531f18c6661db
Christian <c.espinoza.code@gmail.com>
2024-02-18 15:09:13 -0500

    li elementes now are rendered by array

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,17 +1,34 @@
 import Link from 'next/link';
-import { AiFillBug } from "react-icons/ai";
+import { AiFillBug } from 'react-icons/ai';
 
 function NavBar() {
+  const links = [
+    {
+      label: 'Dashboard',
+      href: '/',
+    },
+    {
+      label: 'Issues',
+      href: '/issues',
+    },
+  ];
+
   return (
-    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
-      <Link href="/"><AiFillBug /></Link>
-      <ul className='flex space-x-6'>
-        <li>
-          <Link href="/" className='text-zinc-500 hover:text-zinc-800 transition-colors'>Dashboard</Link>
-        </li>
-        <li>
-          <Link href="/issues" className='text-zinc-500 hover:text-zinc-800'>Issues</Link>Issues
-        </li>
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((element) => (
+          <li key={element.href}>
+            <Link
+              href={element.href}
+              className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            >
+              {element.label}
+            </Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
