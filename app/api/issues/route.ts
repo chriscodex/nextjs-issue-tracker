c5d46bc79b1d91d137f49d4f093b5eecbcdcc443
Christian <c.espinoza.code@gmail.com>
2024-02-20 16:37:16 -0500

    variable assigned to z object

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest } from 'next/server';
 import {z} from 'zod';
 
-z.object({
+const createIssueSchema = z.object({
   title: z.string().min(1).max(255),
   description: z.string().min(1)
 })
