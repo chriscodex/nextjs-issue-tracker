518330ecf0247d0e199535a1e4fe610ef7744dfe
Christian <c.espinoza.code@gmail.com>
2024-02-21 17:17:32 -0500

    radix ui setted up

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,5 +1,11 @@
+import { Flex, Text, Button } from '@radix-ui/themes';
+
 function page() {
-  return <div>Issues Page</div>;
+  return (
+    <div>
+      <Button>New Issue</Button>
+    </div>
+  );
 }
 
 export default page;
