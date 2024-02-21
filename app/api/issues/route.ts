ea6d7bd149c87a28c86762a0cad9403059c13f34
Christian <c.espinoza.code@gmail.com>
2024-02-21 17:11:03 -0500

    fix: return code 400 added

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -10,11 +10,10 @@ const createIssueSchema = z.object({
 export async function POST(request: NextRequest) {
   const body = await request.json();
   const validation = createIssueSchema.safeParse(body);
-  if (!validation.success) {
+  if (!validation.success)
     return NextResponse.json(validation.error.errors, {
       status: 400,
     });
-  }
 
   const newIssue = await prisma.issue.create({
     data: {
