1c0b18df6b776f4e34205d91c85f5e24d76c21f6
Christian <c.espinoza.code@gmail.com>
2024-02-22 18:10:48 -0500

    typografi config added to layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { Theme, ThemePanel } from '@radix-ui/themes';
 
 import { NavBar } from './NavBar';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -20,10 +20,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Theme appearance='light' accentColor='violet'>
+      <body className={inter.variable}>
+        <Theme appearance="light" accentColor="violet">
           <NavBar />
-          <main className='p-5'>{children}</main>
+          <main className="p-5">{children}</main>
         </Theme>
       </body>
     </html>
