adc16e81acb9b570cf706c4031f9c5093250525b
Christian <c.espinoza.code@gmail.com>
2024-02-22 18:00:41 -0500

    new issue button now redirect to a new page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,9 +1,12 @@
 import { Flex, Text, Button } from '@radix-ui/themes';
+import Link from 'next/link';
 
 function page() {
   return (
     <div>
-      <Button>New Issue</Button>
+      <Button>
+        <Link href="/issues/new">New Issue</Link>
+      </Button>
     </div>
   );
 }
