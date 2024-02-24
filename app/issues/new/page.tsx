4cc3455a39f7804e4fe26068ff14eecb4ebd57c3
Christian <c.espinoza.code@gmail.com>
2024-02-24 09:52:13 -0500

    simpleMDE input now uses a controller

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,7 +2,7 @@
 import { Button, TextField } from '@radix-ui/themes';
 import React from 'react';
 import SimpleMDE from 'react-simplemde-editor';
-import { useForm } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 
 import 'easymde/dist/easymde.min.css';
 
@@ -12,14 +12,20 @@ interface IssueForm {
 }
 
 function NewIssuePage() {
-  const { register } = useForm<IssueForm>();
+  const { register, control } = useForm<IssueForm>();
 
   return (
     <div className="max-w-xl space-y-3">
       <TextField.Root>
         <TextField.Input placeholder="Title" {...register('title')} />
       </TextField.Root>
-      <SimpleMDE placeholder="Description" />
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="Description" {...field} />
+        )}
+      />
       <Button>Submit New Issue</Button>
     </div>
   );
