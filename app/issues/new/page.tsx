6728c44fcff1a7341838acd86eefe8a122237f01
Christian <c.espinoza.code@gmail.com>
2024-02-25 10:27:15 -0500

    error state created

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { Button, TextField } from '@radix-ui/themes';
-import React from 'react';
+import React, { useState } from 'react';
 import SimpleMDE from 'react-simplemde-editor';
 import axios from 'axios';
 import { useForm, Controller } from 'react-hook-form';
@@ -16,13 +16,18 @@ interface IssueForm {
 function NewIssuePage() {
   const router = useRouter();
   const { register, control, handleSubmit } = useForm<IssueForm>();
+  const [error, setError] = useState('');
 
   return (
     <form
       className="max-w-xl space-y-3"
       onSubmit={handleSubmit(async (data) => {
-        await axios.post('/api/issues', data);
-        router.push('/issues');
+        try {
+          await axios.post('/api/issues', data);
+          router.push('/issues');
+        } catch (error) {
+          console.error(error);
+        }
       })}
     >
       <TextField.Root>
