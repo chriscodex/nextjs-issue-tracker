414d2f08b74a61b678186d85c140a94ab21d1c16
Christian <c.espinoza.code@gmail.com>
2024-02-25 10:33:40 -0500

    styles callout error added

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -21,7 +21,7 @@ function NewIssuePage() {
   return (
     <div className='max-w-xl'>
       {error && (
-        <Callout.Root color="red">
+        <Callout.Root color="red" className='mb-5'>
           <Callout.Text>{error}</Callout.Text>
         </Callout.Root>
       )}
